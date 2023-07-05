ca413d8b7e1d9c7d38cb8309d0c0800de913c57c
gdumani <gdumani@gmail.com>
2023-07-05 03:11:35 -0600

    Create scaffold for groups and expenses Create  join table between groups and expenses

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :groups
+  resources :expenses
   devise_for :users
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
---
+++ db/migrate/20230705083816_rename_foreign_key_expenses.rb
@@ -0,0 +1,5 @@
+class RenameForeignKeyExpenses < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :expenses, :user_id, :author_id
+  end
+end
