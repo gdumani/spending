97eecda1d16e0676a76ab6ea0b076166a8482438
gdumani <gdumani@gmail.com>
2023-07-07 16:52:07 -0600

    add views to devise

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,23 @@
 class Expense < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  has_and_belongs_to_many :groups
+
+  validates :name, presence: true
+  validates :amount, numericality: true
+
+  def total
+    sum(:amount)
+  end
+
+  def total_for_user(user)
+    where(author: user).total
+  end
+
+  def total_for_group(group)
+    joins(:groups).where(groups: { id: group.id }).total
+  end
+
+  def total_for_user_and_group(user, group)
+    joins(:groups).where(groups: { id: group.id }, author: user).total
+  end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,22 @@
 class Group < ApplicationRecord
   belongs_to :user
+  has_and_belongs_to_many :expenses
+
+  validates :name, presence: true
+
+  def total
+    sum(:amount)
+  end
+
+  def total_for_user(user)
+    where(author: user).total
+  end
+
+  def total_for_group(group)
+    joins(:groups).where(groups: { id: group.id }).total
+  end
+
+  def total_for_user_and_group(user, group)
+    joins(:groups).where(groups: { id: group.id }, author: user).total
+  end
 end
